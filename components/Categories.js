4cca9add6fae441595a52eeb2a1983b74d0a2f8e
Muhammad Sulaiman <sulaimanperson@gmail.com>
2021-12-25 20:06:56 +0500

    gsd

--- components/Categories.js
+++ components/Categories.js
@@ -37,7 +37,7 @@ const Categories = () => {
 						<div className="border-b  border-gray-300" key={currElement.id}>
 							<div className="grid mainCategoryGrid">
              <div className="font-bold text-xl py-2 flex items-center text-orange-400">{currElement.category}</div>
-             <div className="font-bold opacity-70 text-xl py-2 flex items-center">{currElement.numbers}</div>
+             <div className="font-bold opacity-70  text-base py-2 flex items-center">{currElement.numbers}</div>
              <div className="flex py-1">{ currElement.image.map((getImage) =>{
 		return(
 		  <a href={getImage.openseaLink} key={getImage.openseaLink} target="_blank" className="sharkCategoryImage relative block"><img src={getImage.imageLink} alt="Shark Image" /></a>
