c5adadf61039ea81a01d78497b0ba79a46e0df72
Muhammad Sulaiman <sulaimanperson@gmail.com>
2021-12-25 22:24:34 +0500

    asd

--- components/Categories.js
+++ components/Categories.js
@@ -32,7 +32,7 @@ const Categories = () => {
 
 		<div className="mainContCategory w-full">
 			{
-				getdata.map((currElement) =>{
+				getdata.slice(0, 6).map((currElement) =>{
 					return(
 						<div className="border-b  border-gray-300" key={currElement.id}>
 							<div className="grid mainCategoryGrid">
@@ -53,6 +53,46 @@ const Categories = () => {
 				})
 			}
 		</div>
+
+
+
+
+
+    <div className="topCategoryGrid grid w-full border-b pt-5 pb-3 mt-5  border-gray-300">
+    <div className="font-bold opacity-70 text-xl">Accessories</div>
+    </div>
+
+    <div className="topCategoryGrid grid w-full border-b py-5  border-gray-300">
+         <div className="font-bold opacity-70 text-xl ">Attributes</div>
+         <div className="font-bold opacity-70 text-xl ">#</div>
+         <div className="font-bold opacity-70 text-xl ">More Examples</div>
+		</div>
+
+    <div className="mainContCategory w-full">
+			{
+				getdata.slice(6, 20).map((currElement) =>{
+					return(
+						<div className="border-b  border-gray-300" key={currElement.id}>
+							<div className="grid mainCategoryGrid">
+             <div className="font-bold text-xl py-2 flex items-center text-orange-400">{currElement.category}</div>
+             <div className="font-bold opacity-70  text-base py-2 flex items-center">{currElement.numbers}</div>
+             <div className="flex py-1">{ currElement.image.map((getImage) =>{
+		return(
+		  <a href={getImage.openseaLink} key={getImage.openseaLink} target="_blank" className="sharkCategoryImage relative block"><img src={getImage.imageLink} alt="Shark Image" /></a>
+		)
+	})
+ }
+ </div>
+
+
+</div>
+						</div>
+					)
+				})
+			}
+		</div>
+
+
 
         </>
     )
