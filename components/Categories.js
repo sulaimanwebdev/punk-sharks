e06f510ba2b003ae62fafaa961683bd7917af8de
Muhammad Sulaiman <sulaimanperson@gmail.com>
2021-12-27 20:23:12 +0500

    asd

--- components/Categories.js
+++ components/Categories.js
@@ -94,6 +94,85 @@ const Categories = () => {
 
 
 
+
+
+
+
+		<div className="topCategoryGrid grid w-full border-b py-5  pt-7 border-gray-300">
+         <div className="font-bold opacity-70 text-xl ">Rarity Count</div>
+         <div className="font-bold opacity-70 text-xl ">#</div>
+         <div className="font-bold opacity-70 text-xl ">More Examples</div>
+		</div>
+
+    <div className="mainContCategory w-full">
+			{
+				getdata.slice(35, 37).map((currElement) =>{
+					return(
+						<div className="border-b  border-gray-300" key={currElement.id}>
+							<div className="grid mainCategoryGrid">
+             <div className="font-bold py-2 flex items-center text-orange-400 categoryName">{currElement.category}</div>
+             <div className="font-bold opacity-70  text-base py-2 flex items-center">{currElement.numbers}</div>
+             <div className="flex py-1">{ currElement.image.map((getImage) =>{
+		return(
+		  <a href={getImage.openseaLink} key={getImage.openseaLink} target="_blank" className="sharkCategoryImage relative block"><img src={getImage.imageLink} alt="Shark Image" /></a>
+		)
+	})
+ }
+ </div>
+
+
+</div>
+						</div>
+					)
+				})
+			}
+		</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+		<div className="topCategoryGrid grid w-full border-b py-5  pt-7 border-gray-300">
+         <div className="font-bold opacity-70 text-xl ">Rare</div>
+         <div className="font-bold opacity-70 text-xl ">#</div>
+         <div className="font-bold opacity-70 text-xl ">More Examples</div>
+		</div>
+
+    <div className="mainContCategory w-full">
+			{
+				getdata.slice(37, 50).map((currElement) =>{
+					return(
+						<div className="border-b  border-gray-300" key={currElement.id}>
+							<div className="grid mainCategoryGrid">
+             <div className="font-bold py-2 flex items-center text-orange-400 categoryName">{currElement.category}</div>
+             <div className="font-bold opacity-70  text-base py-2 flex items-center">{currElement.numbers}</div>
+             <div className="flex py-1">{ currElement.image.map((getImage) =>{
+		return(
+		  <a href={getImage.openseaLink} key={getImage.openseaLink} target="_blank" className="sharkCategoryImage relative block"><img src={getImage.imageLink} alt="Shark Image" /></a>
+		)
+	})
+ }
+ </div>
+
+
+</div>
+						</div>
+					)
+				})
+			}
+		</div>
+
+
+
+
         </>
     )
 }
