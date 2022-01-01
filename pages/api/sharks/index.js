50495aa1eb731dfca3e4267bf7a6b40492947869
Muhammad Sulaiman <sulaimanperson@gmail.com>
2022-01-01 21:44:56 +0500

    all set and done

--- pages/api/sharks/index.js
+++ pages/api/sharks/index.js
@@ -1,23 +1,58 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import Sharks from './sharks-data.json';
+// import Sharks from './sharks-data.json';
 
 export default function helloAPI(req, res) {
-if(req.method === 'GET'){
+// if(req.method === 'GET'){
 
-let SortData = Sharks.sort((a, b) => a.edition - b.edition);
+// let SortData = Sharks.sort((a, b) => a.edition - b.edition);
  
 
-  res.status(200).json(SortData)
+  res.status(200).json("Something went wrong")
+
+// }
+
+
+
+
+// else{
+//   res.status(401).json('Only "GET" request are allowed')
+// }
+
+
 
 }
 
 
 
 
-else{
-  res.status(401).json('Only "GET" request are allowed')
-}
 
 
 
-}
+
+
+
+// for single id
+
+
+
+
+// [singleShark].js
+
+// import Sharks from './sharks-data.json';
+// export default function helloAPI(req, res) {
+//   let {singleShark} = req.query;
+//   let shark = Sharks.find((shark) => shark.id === parseInt(singleShark));
+
+// if(req.method === 'GET'){
+//   res.status(200).json(shark)
+
+
+// }
+
+
+// else{
+//   res.status(401).json('Only "GET" request are allowed')
+// }
+
+
+// }
