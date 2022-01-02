742930272ca6a2bedccf8701168106ddb30abcc8
Muhammad Sulaiman <sulaimanperson@gmail.com>
2022-01-02 21:55:21 +0500

    asd

--- components/ProvenenceRecord.js
+++ components/ProvenenceRecord.js
@@ -42,8 +42,8 @@ setgetdata(await response.json());
 <div className="provenceGrid provenceGrid-2 py-2">
          <div className="sharkID font-normal text-base opacity-80">{currElement.edition}</div>
           <div className="font-normal text-base opacity-80">{currElement.dna}</div>
-          <a href={`https://opensea.io/assets/matic/0xb536efc10864408a2f491d1a6dfb0e76567c6659/${currElement.edition}`} target="_blank" className="font-normal text-blue-700 hover:text-blue-900 transition text-base opacity-80">View on Opensea</a>
-          <a href={`https://gateway.pinata.cloud/ipfs/Qme4gVeMjq7QBHTikE3bUYHYb3gBCuzgdTyXsARCiTCx69/${currElement.edition}.png`} target="_blank" className="font-normal text-blue-700 hover:text-blue-900 transition text-base opacity-80">View image</a>
+          <a href={`https://opensea.io/assets/matic/0xb536efc10864408a2f491d1a6dfb0e76567c6659/${currElement.edition}`} target="_blank" className="font-normal text-blue-700 hover:text-yellow-500  text-base opacity-80">View on Opensea</a>
+          <a href={`https://gateway.pinata.cloud/ipfs/Qme4gVeMjq7QBHTikE3bUYHYb3gBCuzgdTyXsARCiTCx69/${currElement.edition}.png`} target="_blank" className="font-normal text-blue-700 hover:text-yellow-500  text-base opacity-80">View image</a>
          </div>
 
 
